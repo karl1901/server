0266c2e8730ea4878ee880e7648bda5486889965
karl <2371265527@qq.com>
2021-04-15 08:32:45 +0800

    部署文件新增vue项目部署说明

---
+++ vue配置文件/vue.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  // 生成环境不需要map文件
+  productionSourceMap: !process.env.NODE_ENV === 'production',
+  // 项目输出路径(build后项目保存的路径)
+  outputDir: 'D:\\karl word\\aliyun.code\\test-server\\myvue',
+  // 服务器虚拟路径(判断是否是本地启动还是服务器启动)
+  publicPath: process.env.NODE_ENV === 'production' ? '/myvue/' : '/'
+};
